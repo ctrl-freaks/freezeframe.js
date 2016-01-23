e35383dd390977d3ca774757ec5f6d699d934515
Christopher Antonellis <christopher.antonellis@gmail.com>
2016-01-23 12:36:37 -0500

    changed tabs to spaces

--- gulpfile.js
+++ gulpfile.js
@@ -22,50 +22,50 @@ var rename = require("gulp-rename");
 // Clean ///////////////////////////////////////////////////////////////////////
 
 gulp.task('js-clean', function () {
-	return gulp.src('./build/js/*.js', {read: false})
-		.pipe(clean());
+  return gulp.src('./build/js/*.js', {read: false})
+    .pipe(clean());
 });
 
 // Concatenate Packaged File ///////////////////////////////////////////////////
 
 gulp.task('js-concat', ['js-clean'], function () {
-	
-	gulp.src(['./src/js/freezeframe.js'])
-		.pipe(gulp.dest('./build/js/'));
+  
+  gulp.src(['./src/js/freezeframe.js'])
+    .pipe(gulp.dest('./build/js/'));
 
-	return gulp.src([
-			'./src/js/vendor/jquery-2.1.4.js',
-			'./src/js/vendor/imagesloaded.js',
-			'./src/js/*.js'
-		])
-		.pipe(concat('freezeframe.pkgd.js'))
-		.pipe(gulp.dest('./build/js/'));
+  return gulp.src([
+      './src/js/vendor/jquery-2.1.4.js',
+      './src/js/vendor/imagesloaded.js',
+      './src/js/*.js'
+    ])
+    .pipe(concat('freezeframe.pkgd.js'))
+    .pipe(gulp.dest('./build/js/'));
 });
 
 // Uglify //////////////////////////////////////////////////////////////////////
 
 gulp.task('js-uglify', ['js-concat'], function() {
-	
-	gulp.src(['./build/js/freezeframe.js'])
-		.pipe(uglify())
-		.pipe(rename({
-			suffix: ".min",
-		}))
-		.pipe(gulp.dest('./build/js/'));
+  
+  gulp.src(['./build/js/freezeframe.js'])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min",
+    }))
+    .pipe(gulp.dest('./build/js/'));
 
-	return gulp.src(['./build/js/freezeframe.pkgd.js'])
-		.pipe(uglify())
-		.pipe(rename({
-			suffix: ".min",
-		}))
-		.pipe(gulp.dest('./build/js/'));
+  return gulp.src(['./build/js/freezeframe.pkgd.js'])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: ".min",
+    }))
+    .pipe(gulp.dest('./build/js/'));
 
 });
 
 // Watch ///////////////////////////////////////////////////////////////////////
 
 gulp.task('js-watch', function () {
-	return gulp.watch('./src/js/*.js', ['js-dev']);
+  return gulp.watch('./src/js/*.js', ['js-dev']);
 });
 
 // Tasks ///////////////////////////////////////////////////////////////////////
@@ -82,35 +82,35 @@ gulp.task('js', ['js-uglify']);
 // Clean ///////////////////////////////////////////////////////////////////////
 
 gulp.task('css-clean', function () {
-	return gulp.src('./build/css/*.css', {read: false})
-		.pipe(clean());
+  return gulp.src('./build/css/*.css', {read: false})
+    .pipe(clean());
 });
 
 // SCSS ////////////////////////////////////////////////////////////////////////
 
 gulp.task('css-scss', ['css-clean'], function () {
-	return gulp.src([
-		'./src/scss/*.scss',
-	])
-		.pipe(sass())
-		.pipe(gulp.dest('./build/css/'));
+  return gulp.src([
+    './src/scss/*.scss',
+  ])
+    .pipe(sass())
+    .pipe(gulp.dest('./build/css/'));
 });
 
 // Minify //////////////////////////////////////////////////////////////////////
 
 gulp.task('css-minify', ['css-scss'], function () {
-	return gulp.src('./build/css/*.css')
-		.pipe(minifyCss())
-		.pipe(rename({
-			suffix: ".min",
-		}))
+  return gulp.src('./build/css/*.css')
+    .pipe(minifyCss())
+    .pipe(rename({
+      suffix: ".min",
+    }))
     .pipe(gulp.dest('./build/css'));
 });
 
 // Watch ///////////////////////////////////////////////////////////////////////
 
 gulp.task('css-watch', function () {
-	return gulp.watch('./src/scss/*.scss', ['css-dev']);
+  return gulp.watch('./src/scss/*.scss', ['css-dev']);
 });
 
 // Tasks ///////////////////////////////////////////////////////////////////////
