ef36793ab9b7e186708a7f54e69e4e268f138c00
Nick Ford <nickforddesign@gmail.com>
2019-06-17 14:51:20 -0400

    fix export and clean up examples

--- packages/vue-freezeframe/src/App.vue
+++ packages/vue-freezeframe/src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
-    <freezeframe src="http://static1.squarespace.com/static/51c748abe4b0c275d0aa86bf/56141631e4b0095d43132f02/5614164be4b018c0454fd619/1444744841735/pool980.gif" />
-    <freezeframe src="http://static1.squarespace.com/static/51c748abe4b0c275d0aa86bf/55f96e21e4b0f86680e762dc/56aa551b8b38d446e39e933b/1454003533097/.gif" ref="manual" />
-    <button @click="toggle">Toggle</button>
+    <p class="bold title">vue-freezeframe</p>
+
+    <div class="section">
+      <p>natural width,<br />animate on hover.</p>
+
+      <freezeframe src="http://static1.squarespace.com/static/51c748abe4b0c275d0aa86bf/56141631e4b0095d43132f02/5614164be4b018c0454fd619/1444744841735/pool980.gif" />
+    </div>
+    
+    <div class="section">
+      <p>start/stop manually</p>
+
+      <button @click="start">Start</button>
+      <button @click="stop">Stop</button>
+      <button @click="toggle">Toggle</button>
+
+      <freezeframe src="http://static1.squarespace.com/static/51c748abe4b0c275d0aa86bf/55f96e21e4b0f86680e762dc/56aa551b8b38d446e39e933b/1454003533097/.gif" ref="freeze" />
+    </div>
   </div>
 </template>
 
@@ -22,25 +36,63 @@ export default {
   methods: {
     toggle() {
       if (this.isPlaying) {
-        this.$refs.manual.stop();
+        this.stop();
       } else {
-        this.$refs.manual.start();
+        this.start();
       }
       this.isPlaying = !this.isPlaying;
+    },
+    start() {
+      this.$refs.freeze.start();
+    },
+    stop() {
+      this.$refs.freeze.stop();
     },
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  max-width: 1000px;
+  margin: 0px auto;
+  padding: 20px;
+  font-size: 13px;
   text-align: center;
-  color: #2c3e50;
-  width: 800px;
-  max-width: 100%;
-  margin: 60px auto;
+  color: #202020;
+}
+
+a {
+  color: #202020;
+}
+
+p {
+  padding: 10px 0px;
+  text-align: center;
+  font-family: monospace;
+}
+
+button {
+  width: 100px;
+  margin: 0px 10px 10px 0px;
+  padding: 5px 0px;
+  border: 1px solid #000;
+  background: #fff;
+  outline: none;
+  font-family: monospace;
+  text-align: center;
+}
+
+button:active {
+  background: #000;
+  color: #fff;
+}
+
+.bold {
+  font-weight: 700;
+}
+
+.italic {
+  font-style: italic;
 }
 </style>
