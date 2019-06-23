6f7c811ef56e0d6c638dfb7660974397b3f71fdc
Nick Ford <nickforddesign@gmail.com>
2019-06-23 11:35:19 -0400

    got example test to run

--- packages/freezeframe/jest.config.js
+++ packages/freezeframe/jest.config.js
@@ -9,18 +9,18 @@ module.exports = {
   transformIgnorePatterns: [
     '/node_modules/'
   ],
-  // moduleNameMapper: {
-  //   '^@/(.*)$': '<rootDir>/src/$1'
-  // },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
   // snapshotSerializers: [
   //   'jest-serializer-vue'
   // ],
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/',
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname'
-  ]
+  testURL: 'http://localhost/'
+  // watchPlugins: [
+  //   'jest-watch-typeahead/filename',
+  //   'jest-watch-typeahead/testname'
+  // ]
 };
---
+++ packages/freezeframe/tests/unit/freezeframe.spec.js
@@ -0,0 +1,7 @@
+import freezeframe from '../../src/index';
+
+console.log(freezeframe);
+
+test('that one is one', () => {
+  expect(1).toBe(1);
+});
