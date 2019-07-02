0e105480992baa0e2dee06caa6d107fdee625f87
Nick Ford <nickforddesign@gmail.com>
2019-07-02 10:55:39 -0400

    more generic mock name

--- packages/freezeframe/tests/unit/freezeframe.spec.js
+++ packages/freezeframe/tests/unit/freezeframe.spec.js
@@ -1,8 +1,8 @@
 import Freezeframe from '../../src/index';
 import { classes } from '../../src/constants';
-import mockCanvas from '../mocks/canvas';
+import mockDomApis from '../mocks/dom';
 
-mockCanvas(window);
+mockDomApis(window);
 
 const testFreezeframeInstance = (imgCount, selector, options, cb = () => {}) => {
   const ff = new Freezeframe(selector, options);
