49993a98e6bfe91e658f7d2ca5eed7e81e7a57ac
Nick Ford <nickforddesign@gmail.com>
2019-07-10 17:30:28 -0400

    fixed gh-pages paths

---
+++ docs/react/precache-manifest.a631062c8219867bc2906decaa332deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495d9faa7ae55ebf00aaa0875936f072",
+    "url": "/freezeframe.js/react/index.html"
+  },
+  {
+    "revision": "48e370032bab058106b7",
+    "url": "/freezeframe.js/react/static/css/main.85c39dd4.chunk.css"
+  },
+  {
+    "revision": "bb8a4a0c05535095512e",
+    "url": "/freezeframe.js/react/static/js/2.7ff973e6.chunk.js"
+  },
+  {
+    "revision": "48e370032bab058106b7",
+    "url": "/freezeframe.js/react/static/js/main.7b514271.chunk.js"
+  },
+  {
+    "revision": "84cb35a47ccb251394a0",
+    "url": "/freezeframe.js/react/static/js/runtime~main.0b442b39.js"
+  }
+]);
--- docs/react/service-worker.js
+++ docs/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/freezeframe.js/precache-manifest.04531ad88821430a67ff77c5adbc8e9b.js"
+  "/freezeframe.js/react/precache-manifest.a631062c8219867bc2906decaa332deb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/freezeframe.js/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/freezeframe.js/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
