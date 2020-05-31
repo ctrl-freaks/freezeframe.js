bcf1192517e657621a1671e89ecd76d6100fedfa
Nick Ford <nickforddesign@gmail.com>
2020-05-31 15:57:24 -0400

    fix jest config

--- packages/freezeframe/jest.config.js
+++ packages/freezeframe/jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
     '/node_modules/'
   ],
   moduleNameMapper: {
+    '\\.scss$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/tests/assetsTransformer.js'
